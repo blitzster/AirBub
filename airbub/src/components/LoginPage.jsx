ff93eaa13c4de2a67160425671d76127e4ba304f
Akshay S <blitz122333@gmail.com>
2024-10-13 22:37:29 +0530

    authentication modification left

--- airbub/src/components/LoginPage.jsx
+++ airbub/src/components/LoginPage.jsx
@@ -1,13 +1,42 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { Link, Navigate, useSearchParams } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
 export default function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    const [setUser] = useContext(UserContext);
+
+    async function handleLoginSubmit(ev) {
+        ev.preventDefault();
+        try {
+            const { data } = await axios.post('/login', { email, password });
+            setUser(data);
+            alert("Login Successful");
+            setRedirect(true);
+        } catch (e) {
+            alert("Login Failed");
+        }
+    }
+
+    if (redirect) {
+        return <Navigate to={'/'} />
+    }
+
+
     return (
         <div className="mt-4 grow flex items-center justify-center">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Login</h1>
-                <form className="max-w-md mx-auto">
-                    <input type="email" placeholder="your@mail.com" />
-                    <input type="password" placeholder="password" />
+                <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+                    <input type="email" placeholder="your@mail.com"
+                        value={email}
+                        onChange={ev => setEmail(ev.target.value)} />
+                    <input type="password" placeholder="password"
+                        value={password}
+                        onChange={ev => setPassword(ev.target.value)} />
                     <button className="prime">Login</button>
 
                     <div className="text-center py-2 text-gray-500">
